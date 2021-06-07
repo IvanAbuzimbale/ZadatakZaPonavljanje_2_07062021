303cd7e7167d8ee26deeaddc438ea4320e6aa691
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-06-07 09:09:14 +0200

    Add files via upload

---
+++ ZadatakZaPonavljanje_2_07062021/ZadatakZaPonavljanje_2_07062021/KlasaD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakZaPonavljanje_2_07062021
+{
+    class KlasaD
+    {
+        static public bool Suprotno (bool suprotno)
+        {
+            return !suprotno;
+        }
+
+        static public int Suprotni (int suprotni)
+        {
+            int suprotna = 0;
+            int broj = -1;
+            suprotna = broj * suprotni;
+            return suprotna;
+        } 
+    }
+}
---
+++ ZadatakZaPonavljanje_2_07062021/ZadatakZaPonavljanje_2_07062021/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakZaPonavljanje_2_07062021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string argument1;
+            string broj;
+            Console.Write("True ili false? ");
+            argument1 = Console.ReadLine();
+            Console.Write("Upiši neki broj: ");
+            broj = Console.ReadLine();
+            Console.WriteLine("Ha! Nije nego je " + KlasaD.Suprotno(Convert.ToBoolean(argument1)));
+            Console.WriteLine("Vaš broj u suprotnosti je: " + KlasaD.Suprotni(Convert.ToInt32(broj)));
+            Console.ReadKey();
+        }
+    }
+}
